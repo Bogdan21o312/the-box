6a207253bcf93c9d52174fc994dbd39e1d2bcd47
Bogdan21o312 <lutskiv2010@gmail.com>
2023-06-07 16:38:01 +0300

    init

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,7 +1,4 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import {ReactNode} from "react";
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,11 +8,11 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
