c3947e5b79113be3bd59acbdce0e880576386dc7
Bogdan21o312 <lutskiv2010@gmail.com>
2023-06-20 16:38:03 +0300

    Update

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import {ModuleMainScreen, ModuleAdvantages, ModuleAbout} from "./widgets";
+import ImageUploader from "@/app/(pages)/(home)/ImageUploader";
 export default function Home() {
 
     return (
         <>
-            <ModuleMainScreen/>
+            <ImageUploader/>
+            {/*<ModuleMainScreen/>*/}
             <ModuleAdvantages/>
             <ModuleAbout/>
         </>
